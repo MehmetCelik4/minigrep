bb493fa9b1310112d2109b4b0e02b0a03e0c8e86
MehmetCelik <mehmetcelik4@gmail.com>
2021-09-23 13:31:05 +0300

    add binary upload

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use std::env;
-use std::process;
-
-use minigrep::Config;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
-    
-    let config = Config::new(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    
-    if let Err(e) = minigrep::run(config){
-        eprintln!("Application error: {}", e);
-        process::exit(1);
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
     }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
-
-
-
